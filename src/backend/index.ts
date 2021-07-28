e9c0f810d3fd39200ce89893ad2a93e2843a93cd
Laurent Schwitter <laurent.schwitter@gmail.com>
2021-07-28 15:43:53 +0200

    auto build test

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -24,7 +24,7 @@ const authProvider = new RefreshableAuthProvider(new StaticAuthProvider(CREDENTI
         let newCredentials = Object.assign(CREDENTIALS, {})
         newCredentials.oauth = accessToken
         newCredentials.refreshToken = refreshToken
-        newCredentials.expiration = expiryDate.getTime()
+        newCredentials.expiration = expiryDate?.getTime()
         await fs.writeFileSync('twitch_credentials.json', JSON.stringify(newCredentials, null, 4))
     }
 })
