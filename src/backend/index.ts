daabfdf0454f611f077490b075ad9dac1f10b2ee
Laurent Schwitter <laurent.schwitter@gmail.com>
2021-07-29 21:25:28 +0200

    format document

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,4 +1,3 @@
-
 import { ClientCredentialsAuthProvider, RefreshingAuthProvider } from '@twurple/auth'
 import * as fs from 'fs'
 
@@ -21,21 +20,23 @@ export const MONGO = JSON.parse('' + fs.readFileSync('mongo_credentials.json'))
 export const ENDPOINT = JSON.parse('' + fs.readFileSync('endpoint_credentials.json'))
 
 export const clientProvider = new ClientCredentialsAuthProvider(CREDENTIALS.clientId, CREDENTIALS.clientSecret)
-export const userProvider = new RefreshingAuthProvider({
-  clientId: CREDENTIALS.clientId,
-  clientSecret: CREDENTIALS.clientSecret,
-  onRefresh: token => {
-    let newToken = JSON.parse('' + fs.readFileSync('twitch_credentials.json'))
+export const userProvider = new RefreshingAuthProvider(
+  {
+    clientId: CREDENTIALS.clientId,
+    clientSecret: CREDENTIALS.clientSecret,
+    onRefresh: (token) => {
+      let newToken = JSON.parse('' + fs.readFileSync('twitch_credentials.json'))
 
-    if(token.accessToken) newToken.accessToken
-    if(token.refreshToken) newToken.refreshToken
-    if(token.expiresIn) newToken.expiresIn
-    if(token.obtainmentTimestamp) newToken.obtainmentTimestamp
+      if (token.accessToken) newToken.accessToken
+      if (token.refreshToken) newToken.refreshToken
+      if (token.expiresIn) newToken.expiresIn
+      if (token.obtainmentTimestamp) newToken.obtainmentTimestamp
 
-    fs.writeFileSync('twitch_credentials.json', JSON.stringify(newToken, null, 4))
-  }
-}, CREDENTIALS)
-
+      fs.writeFileSync('twitch_credentials.json', JSON.stringify(newToken, null, 4))
+    },
+  },
+  CREDENTIALS
+)
 
 export var channelID
 
@@ -50,67 +51,5 @@ let startApp = async () => {
 
   await Mongo.connect()
   new Storeable()
-
 }
 startApp()
-
-
-
-
-
-
-
-//DELETE WHATS BELOW
-
-
-
-
-/* 
-const authProvider = new RefreshableAuthProvider(new StaticAuthProvider(CREDENTIALS.clientId, CREDENTIALS.accessToken), {
-  clientSecret: CREDENTIALS.clientSecret,
-  refreshToken: CREDENTIALS.accessToken,
-  expiry: CREDENTIALS.obtainmentTimestamp + CREDENTIALS.expiresIn,
-  onRefresh: async ({ accessToken, refreshToken, expiryDate }) => {
-    console.log('---------------------------------- NEW TOKEN DATA RECEIVED (INDEX) ----------------------------------')
-    let newCredentials = JSON.parse(`${fs.readFileSync('twitch_credentials.json')}`)
-    newCredentials.accessToken = accessToken
-    newCredentials.refreshToken = refreshToken
-    newCredentials.expiresIn = expiryDate?.getTime() - Date.now()
-    newCredentials.obtainmentTimestamp = Date.now()
-    fs.writeFileSync('twitch_credentials.json', JSON.stringify(newCredentials, null, 4))
-  },
-})
-
-
-const client = new tmi.Client({
-  options: {
-    debug: true,
-    messagesLogLevel: 'error',
-  },
-  connection: {
-    reconnect: true,
-    secure: true,
-  },
-  authProvider: authProvider,
-  channels: [CREDENTIALS.channel],
-})
-
-client
-  .disconnect()
-  .then((value) => {
-
-    new Common(client)
-    new Everyone(client)
-    new Moderators(client)
-
-    let socket = new Socket(PORT, [])
-    //@ts-ignore
-    client.socket = socket.io
-
-    new Mongo(() => {
-      new Saved(client)
-    })
-  })
-  .catch((reason) => {
-    console.log(`Could not connect to twitch chat for reason: ${reason}`)
-  }) */
