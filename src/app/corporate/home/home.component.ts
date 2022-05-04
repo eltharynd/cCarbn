e11bb646cbd70ff27e0e12e7b6f066712c92263b
Laurent Schwitter <laurent.schwitter@gmail.com>
2022-05-04 20:07:53 +0200

    asd

--- src/app/corporate/home/home.component.ts
+++ src/app/corporate/home/home.component.ts
@@ -1,10 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations'
 import { Component } from '@angular/core';
 import { AuthGuard } from 'src/app/auth/auth.guard'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition('void=>*', [
+        style({opacity: 0, transform: 'translate(-.3rem, .4rem)'}),
+        animate('1500ms ease', style({}))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
 
