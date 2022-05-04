39069b9cee6d2b7755aff22e6dbd83e1c49dc35d
Laurent Schwitter <laurent.schwitter@gmail.com>
2022-05-04 23:16:08 +0200

    asd

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme'
+import { DeviceDetectorService } from 'ngx-device-detector'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private sidebarService: NbSidebarService) {}
+  constructor(private sidebarService: NbSidebarService, private devide: DeviceDetectorService) {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.sidebarService.expand()
-    }, 1000);
+    if(this.devide.isDesktop())
+      setTimeout(() => {
+        this.sidebarService.expand()
+      }, 1000);
   }
 }
