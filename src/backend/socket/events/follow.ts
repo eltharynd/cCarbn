df82adcfc8a4b30681928c7a9b610e6f2153f9b7
Laurent Schwitter <laurent.schwitter@gmail.com>
2022-05-15 17:54:49 +0200

    hotfix follow event

--- src/backend/socket/events/follow.ts
+++ src/backend/socket/events/follow.ts
@@ -10,8 +10,6 @@ export class FollowHandler {
     data.userInfo = getUserInfo(await event.getUser())
 
     let found: any = await User.findOne({ twitchId: event.broadcasterId })
-    if (found) {
-      Socket.io.to(found._id.toString()).emit('alerts', toJSON(event))
-    }
+    if (found) Socket.io.to(found._id.toString()).emit('alerts', toJSON(event))
   }
 }
